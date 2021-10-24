2931723a3ef01d48c6266657d19642bca9cc2420
Omar Mohamed Abdel-latif Mohamed <omar.mohamed.latif@gmail.com>
2021-10-24 19:55:42 +0200

    -add script to parse the reports

---
+++ parse_reports.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import docx2txt
+from glob import glob
+import re
+import os
+
+patient_reports = {}
+
+def parse_report(patient_id, report_lines):
+    global patient_reports
+    mode = ''
+    side = ''
+    for line in report_lines:
+        if line[:4] == 'acr ' or line[:11] =='patient no.':
+            continue
+        if line == 'left breast:':
+            side = 'L'
+        elif line == 'right breast:':
+            side = 'R'
+        elif line == 'contrast enhanced spectral mammography revealed:':
+            mode = 'CM'
+        elif line == 'digitalized low dose soft tissue mammography revealed:':
+            mode = 'DM'
+        elif line == 'opinion:':
+            mode = 'DM_OPINION'
+        else:
+            image_name = f"P{patient_id}_{side}_{mode}"
+            if image_name in patient_reports.keys():
+                patient_reports[image_name] += ' ' + line
+            else:
+                patient_reports[image_name] = line
+
+report_csv = {"Image_name":[],"report":[]}
+
+for filepath in glob('./reports/Medical reports/*.docx'):
+    if os.path.basename(filepath)[0]=='~':
+        continue
+    print(filepath)
+    report = docx2txt.process(filepath)
+    patient_id = os.path.basename(filepath)[1:-5]
+    # lines = report.split('\n')
+    report_lines = [line.strip().lower() for line in report.split('\n') if line.strip() != '']
+    parse_report(patient_id, report_lines)
+
+dataset_df = pd.read_excel('./reports/annotations.xlsx')
+
+for index, row in dataset_df.iterrows():
+    image_name = row['Image_name']
+    report_csv['Image_name'].append(image_name)
+    report = 'startseq ' + patient_reports['_'.join(image_name.split('_')[:3])] + ' endseq'
+    report_csv['report'].append(report)
+
+
+df=pd.DataFrame(report_csv)
+
+df.to_csv(os.path.join("./reports","reports.csv"), index=False)
+
+
+
+
+
